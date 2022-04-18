e865610aa8aa7e7176e643dc2a820d7ab73bc1e7
Alejandro Ramirez <alejandroramirez688@Gmail.com>
2022-04-18 18:58:43 -0500

    Add project files.

---
+++ DesignPatternsApp/Configuration/MyConfig.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DesignPatternsApp.Configuration
+{
+    public class MyConfig
+    {
+        public string PathLog { get; set; }
+    }
+}
